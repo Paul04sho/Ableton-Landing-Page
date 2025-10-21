c052cd1f2bc11bdcdb3581cc1c6359d69d22fec3
Paul-Em. <168630301+Paul04sho@users.noreply.github.com>
2025-10-21 15:49:40 +0000

    Implement collapsible content toggle feature
    Added functionality to toggle collapsible content.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+// Select elements
+const moreContent = document.querySelector('.collapsible-content-container');
+const moreBtn = document.querySelector('.collapsible-toggle');
+const toggleIcon = document.querySelector('.bx-plus');
 
+let collapsibleOpen = false;
+
+// Function to toggle the More + button
+function handleCollapsible () {
+    collapsibleOpen = !collapsibleOpen;
+    moreContent.classList.toggle('open-collapsible');
+
+   if(collapsibleOpen) {
+    toggleIcon.classList.add("bx-minus");
+    toggleIcon.classList.remove("bx-plus");
+   } else {
+        toggleIcon.classList.remove("bx-minus");
+        toggleIcon.classList.add("bx-plus");
+   }
+}
